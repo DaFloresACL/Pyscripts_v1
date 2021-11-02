85670dc847e53c3214fc8b3523bfcbcf354c5533
DaFloresACL <DaFlores@americashloans.net>
2021-11-02 15:57:09 -0500

    11.02.2021

---
+++ Pyscripts_v1/EOM_LoanProd_Spreadsheet.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+import os
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
+import datetime 
+import calendar
+import keyring
+
+
+username = 'prodSQLFinUserRO'
+password = keyring.get_password('ACL.LMS',username)
+
+today = datetime.datetime.today()
+last_month = datetime.datetime.strptime(datetime.datetime.strftime(today + relativedelta(months=-1), '%Y-%m-01'),'%Y-%m-%d') + relativedelta(days=0)
+last_day_of_last_month = calendar.monthrange(last_month.year,last_month.month)[-1]
+
+last_month_eom = datetime.datetime.strptime(datetime.datetime.strftime(last_month + relativedelta(days=(last_day_of_last_month-1)  ), '%Y-%m-%d'),'%Y-%m-%d') + relativedelta(days=0)
+
+eom = datetime.datetime.strftime(last_month_eom,'%Y-%m-%d')
+
+engine = sqlalchemy.create_engine(f'mssql+pyodbc://{username}:{password}@secondaryacl.database.windows.net/acl.lms?driver=ODBC+Driver+17+for+SQL+Server')
+connection = engine.connect()
+
+query = f'''
+SET NOCOUNT ON
+
+declare @EOM date = eomonth('2018-02-28',0)
+if object_id('tempdb..#Loans') is not null drop table #Loans
+select	LoanID
+INTO	#Loans
+FROM	Loans a
+WHERE	a.LoanDisbursementStatus = 2 -- funds are disbursed
+and	convert(date,rpt.ToCst(a.DisbursementDate)) <= @EOM -- funds disbursed before EOM
+and	isnull(rpt.ToCst(a.VoidedDate),'2099-12-31') > @EOM -- not voided or voided after EOM
+and	isnull(rpt.ToCst(a.RescindedDate),'2099-12-31') > @EOM -- not rescinded or rescinded after EOM
+Create	Clustered INDEX [IX_Loans] on #Loans (LoanID asc)
+
+
+
+if object_id('tempdb..#MaxPay') is not null drop table #MaxPay
+create	table #MaxPay (LoanID varchar(36), LastPayDate datetime)
+insert	into #MaxPay
+select	b.LoanID
+	, max(TransactionDate) as LatestDate
+from	#Loans a
+Inner	join LoanTransactions b on a.LoanID = b.LoanID
+where	1 not in (HasBeenOffset, IsAnOffset, HasBeenReversed)
+and	TransactionTypeID in (6,7,8,9,10,26,36,42,57)
+group by b.LoanID
+
+
+select	@EOM [EOM]
+	, h.Name [TenantName]
+	, g.Abbreviation [ST]
+	, c.Name [ProductName]
+	, i.Name [ProductType]
+	, a.LoanNo [LoanNo]
+	, upper(b.Name_LastName + ', ' + b.Name_FirstName) [Customer]
+	, d.Number [Loc]
+	, a.APR
+	, convert(date,rpt.ToCst(a.OriginationDate)) [Date]
+	, convert(date,rpt.ToCst(a.DisbursementDate)) [Funded]
+	, e.LoanAmount [Amount]
+	, isnull(f.CurrentPrincipal,0) [CurrentPrincipal]
+	, isnull(f.CurrentInterest,0) [CurrentInterest]
+	, isnull(f.CurrentFees,0) [CurrentFees]
+	, isnull(f.CurrentPrincipal + f.CurrentInterest + f.CurrentFees,0) [CurrentBalance]
+	, f.IsPastDue [IsPastDue]
+	, f.DaysPastDue [DaysPastDue]
+	, f.AmountPastDue [AmountPastDue]
+	, convert(date,rpt.ToCst(a.ChargedOffDate)) [ChargedOffDate]
+	, convert(date,rpt.ToCst(a.PaidInFullDate)) [PaidInFullDate]
+	, j.LastPayDate
+
+
+from	#Loans loan
+	inner join Loans a on loan.LoanID = a.LoanID
+	inner join Customers b on b.CustomerID = a.CustomerID
+	inner join Products c on c.ProductID = a.ProductID
+	inner join Locations d on d.LocationID = a.OriginatingLocationID
+	inner join LoanApplications e on e.LoanID = a.LoanID
+	inner join HistoryMonthEndBalances f on f.LoanID = a.LoanID
+	inner join UnitedStates g on g.StateID = d.StateOfBusinessID
+	inner join Tenants h on h.TenantID = b.TenantID
+	inner join AccountingClassificationTypes i on i.ID = c.AccountingClassificationTypeID
+	left join #MaxPay j on j.LoanID = a.LoanID
+
+where	a.LoanDisbursementStatus = 2 -- funds are disbursed
+and	convert(date,rpt.ToCst(a.DisbursementDate)) <= @EOM -- funds disbursed before EOM
+and	isnull(rpt.ToCst(a.VoidedDate),'2099-12-31') > @EOM -- not voided or voided after EOM
+and	isnull(rpt.ToCst(a.RescindedDate),'2099-12-31') > @EOM -- not rescinded or rescinded after EOM
+and	f.MonthEOM = month(@EOM) 
+and	f.YearEOM = year(@EOM)
+
+order by
+	h.Name
+	, g.Abbreviation
+	, c.Name
+	, a.LoanNo
+
+--drop table #MaxPay'''
+
+df = pd.read_sql(query, connection)  # PROD PURPOSES ONLY
+connection.close()
+
+#os.chdir(r'R:\2021\21-10')
+os.chdir(r'R:\\')
+main_dir = r'R:'
+year_dir = fr'{main_dir}\{eom[0:4]}'
+current_dir = fr'{year_dir}\{eom[2:7]}'
+current_dir
+try:
+	os.mkdir(fr'{current_dir}\\')
+except:
+	pass
+os.chdir(current_dir)
+os.getcwd()
+
+df.to_excel(f'EOM_Loans_{eom[0:7]}.xlsx')
--- Pyscripts_v1/PACER_API.py
+++ Pyscripts_v1/PACER_API.py
@@ -6,6 +6,7 @@ import urllib3
 import pyodbc
 import pandas as pd
 import base64
+import datetime
 #import modin.pandas as pd
 LMS_db_config = 'DRIVER={SQL Server};SERVER=AC-PC-097;DATABASE=FinanceTeamDB;Trusted_Connection=yes;'
 cnxn = pyodbc.connect(LMS_db_config)
@@ -36,24 +37,74 @@ cnxn = pyodbc.connect(LMS_db_config)
 #    ))
 
 #pretty_print_POST(prepared)
+def get_auth_token():
+    auth_url = 'qa-login.uscourts.gov'
+    pacer_url = 'pacer.login.uscourts.gov'
+    username = 'americashloans'
+    password = r'i4b4g63r6FVZc!f'
+    testclientcode = 'testclientcode'
+    auth_post_url = f"https://{auth_url}/services/cso-auth"
+    pacer_post_url = f"https://{auth_url}/services/cso-auth"
 
-auth_url = 'qa-login.uscourts.gov'
-pacer_url = 'pacer.login.uscourts.gov'
-username = 'americashloans'
-password = r'i4b4g63r6FVZc!f'
-testclientcode = 'testclientcode'
-auth_post_url = f"https://{auth_url}/services/cso-auth"
-pacer_post_url = f"https://{auth_url}/services/cso-auth"
+    login_dict = {'loginId': username,'password':password, "redactFlag":1,}
 
-login_dict = {'loginId': username,'password':password, "redactFlag":1,}
+    headers_dict = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    encoded_body = json.dumps(login_dict,ensure_ascii=False)
 
-headers_dict = {'Content-type': 'application/json', 'Accept': 'application/json'}
-encoded_body = json.dumps(login_dict,ensure_ascii=False)
+    #Using requests problems with "!" character
+    r = requests.post(auth_post_url,headers = headers_dict, data = encoded_body.encode('utf-8') 
+                      )
+    auth_dict = r.json()
+    auth_token = auth_dict['nextGenCSO']
+    return auth_token
 
-#Using requests problems with "!" character
-r = requests.post(auth_post_url,headers = headers_dict, data = encoded_body.encode('utf-8') 
-                  )
-auth_dict = r.json()
-auth_token = auth_dict['nextGenCSO']
+auth_token = get_auth_token()
+pcl_url = 'qa-pcl.uscourts.gov'
 
+pcl_headers = {'Content-type': 'application/json', 'Accept': 'application/json', "X-NEXT-GEN-CSO" : auth_token}
+pcl_case_search = { "caseTitle": "Jacob" }
 
+{
+"lastName": "Henderson",
+"firstName": "Nicholas",
+"exactNameMatch": False,
+"courtCase": {
+"jurisdictionType": "bk",
+"caseType": [
+"cv", "ncrim", "misc"
+],
+
+"courtId": [
+"ilcbk", "ilcdc"
+],
+"dateFiledFrom": "2000-01-01",
+"dateFiledTo": "2020-01-01",
+"dateTermedFrom": "2000-01-01",
+"dateTermedTo": "2020-01-01",
+"dateDismissedFrom": "2000-01-01",
+"dateDismissedTo": "2020-01-01",
+"dateDischargedFrom": "2000-01-01",
+"dateDischargedTo": "2020-01-01",
+"federalBankruptcyChapter": [
+"7", "15"
+],
+"natureOfSuit": [
+"140", "151"
+]
+},
+"searchName": "Henderson",
+"searchType": "PARTY"
+}
+
+casefind_url = f'https://{pcl_url}/pcl-public-api/rest/parties/find'
+
+casenum = {
+"lastName": "Henderson",
+}
+case_body = json.dumps(casenum,ensure_ascii=False)
+
+case = requests.post(casefind_url,headers = pcl_headers, data = case_body) 
+
+info = case.json()
+a = datetime.datetime.today()
+b =  datetime.datetime.today()
